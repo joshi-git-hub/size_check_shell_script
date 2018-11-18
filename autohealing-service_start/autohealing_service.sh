0d2c88715812ec2d72fd722e29ad05983e79bc3a
root <root@pagent.example.com>
2018-11-19 01:10:20 +0530

    This is my second comment. now if package not available error comes so it will ask you to install packages.

--- autohealing-service_start/autohealing_service.sh
+++ autohealing-service_start/autohealing_service.sh
@@ -19,6 +19,17 @@ echo "httpd service is running fine."
 else
 {
 echo "There is some issue. might be package is not available. Please check manually."
+read -p "do you want to install the packages for the httpd service. Y/N:-  "  b
+if [ $b == Y ]
+then
+sh /usr/local/script/pack_install.sh
+elif [ $b == N ]
+then
+echo "Thats all right. I quit."
+break
+else
+echo "Please press either Y for yes or N for no."
+fi 
 }
 fi
 }
